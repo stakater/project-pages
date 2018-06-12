7dcbbb8b2f2d3731f8eaf1eb60d1f8f45c1fc30e
Ahmad Iqbal Ali <ahmad@aurorasolutions.io>
2018-06-12 11:15:07 +0500

    changes 20180612

--- js/sliderfun.js
+++ js/sliderfun.js
@@ -6,4 +6,20 @@ $(document).ready(function()
     pause: true,
     interval: 6000,
   });
+
+
+   $('#primary-nav')
+    .on('shown.bs.collapse', function() 
+    {
+      $('#navbar-hamburger').addClass('hidden');
+
+      $('#navbar-close').removeClass('hidden');    
+    })
+
+    .on('hidden.bs.collapse', function() 
+    {
+      $('#navbar-hamburger').removeClass('hidden');
+      $('#navbar-close').addClass('hidden');        
+    });
+
 });
